480999aef59f1c810e9ee5adfa73e3f325bde4d2
Hayaoru <73983834+Hayaoru@users.noreply.github.com>
2021-03-04 13:08:02 +0900

    カテゴリ訂正

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,9 @@
 class Category < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: '' },
-    { id: 3, name: '政治' },
-    { id: 4, name: '' },
+    { id: 2, name: '購入方法' },
+    { id: 3, name: 'サイト' },
+    { id: 4, name: 'アプリ' },
     { id: 5, name: '特別割引' },
     { id: 6, name: '期間限定' },
     { id: 7, name: '特典' },
